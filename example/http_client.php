c4895efd0a66efc4b4bade53671c39e211ece728
elarity <42677833@qq.com>
2018-04-16 12:02:44 +0800

    添加http和tcp客户端代码演示

---
+++ example/http_client.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ 利用curl封装的简单对http的客户端演示案例
+*/
+
+// SW
+$sw = array(
+  'type' => 'SW',
+  'requestId' => time(),
+  'param' => array(
+    'model' => 'Account',
+    'method' => 'login',
+    'param' => array(
+      'username' => 'wahah',
+      'password' => 'wahah',
+    ),
+  ),
+);
+// SN
+$sn = array(
+  'type' => 'SN',
+  'requestId' => time(),
+  'param' => array(
+    'model' => 'Account',
+    'method' => 'login',
+    'param' => array(
+      'username' => 'wahah',
+      'password' => 'wahah',
+    ),
+  ),
+);
+// MW
+$mw = array(
+  'type' => 'MW',
+  'requestId' => time(),
+  'param' => array(
+    'xas' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+    'xxx' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+  ),
+);
+// MN
+$mn = array(
+  'type' => 'MN',
+  'requestId' => time(),
+  'param' => array(
+    'xas' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+    'xxx' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+  ),
+);
+
+
+
+$jsonData = json_encode( $sn ).'\r\n';
+$curl = curl_init();
+curl_setopt_array( $curl, array(
+  CURLOPT_PORT => 9802,
+  CURLOPT_URL => "http://127.0.0.1:9802/",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => $jsonData,
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "postman-token: efe52804-aa89-8c4d-01ae-5e9a27012312"
+  ),
+));
+$response = curl_exec( $curl );
+$err = curl_error( $curl );
+curl_close($curl);
+if ( $err ) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+
---
+++ example/tcp_client.php
@@ -0,0 +1,92 @@
+<?php
+/*
+利用swoole client封装的简单的tcp客户端演示案例
+*/
+
+// SW
+$sw = array(
+  'type' => 'SW',
+  'requestId' => time(),
+  'param' => array(
+    'model' => 'Account',
+    'method' => 'login',
+    'param' => array(
+      'username' => 'wahah',
+      'password' => 'wahah',
+    ),
+  ),
+);
+// SN
+$sn = array(
+  'type' => 'SN',
+  'requestId' => time(),
+  'param' => array(
+    'model' => 'Account',
+    'method' => 'login',
+    'param' => array(
+      'username' => 'wahah',
+      'password' => 'wahah',
+    ),
+  ),
+);
+// MW
+$mw = array(
+  'type' => 'MW',
+  'requestId' => time(),
+  'param' => array(
+    'xas' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+    'xxx' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+  ),
+);
+// MN
+$mn = array(
+  'type' => 'MN',
+  'requestId' => time(),
+  'param' => array(
+    'xas' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+    'xxx' => array(
+      'model' => 'Account',
+      'method' => 'login',
+      'param' => array(
+        'username' => 'wahah',
+        'password' => 'wahah',
+      ),
+    ),
+  ),
+);
+
+
+
+$host = '0.0.0.0';
+$port = 9801;
+$client = new swoole_client( SWOOLE_SOCK_TCP );
+if ( !$client->connect( $host, $port, -1 ) ){
+  exit( "connect failed. Error: {$client->errCode}\n" );
+}
+$client->send( json_encode( $mn ).'\r\n' );
+echo $client->recv();
+$client->close();
+
+
+
